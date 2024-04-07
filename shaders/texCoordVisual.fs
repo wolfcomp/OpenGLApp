dbbbb37c8e2df7026ab372fbc05d53aed3cc141f
wolfcomp <magnus1997.mho@gmail.com>
2024-04-07 19:11:22 +0200

    fix shader not using correct inputs

--- shaders/texCoordVisual.fs
+++ shaders/texCoordVisual.fs
@@ -1,7 +1,15 @@
 #version 330 core
 out vec4 FragColor;
-in vec2 TexCoords;
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+    vec3 Normal;
+    vec4 FragPosLightSpace;
+    mat3 TBN;
+} fs_in;
 
 void main() {
-    FragColor = vec4(TexCoords, 0, 1.0);
+    FragColor = vec4(fs_in.TexCoords, 0, 1.0);
 }
