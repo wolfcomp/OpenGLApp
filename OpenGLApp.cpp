a1295e303aa19e37cbc03209bbd82936c9a399dd
wolfcomp <magnus1997.mho@gmail.com>
2024-04-22 10:22:35 +0200

    change file extension to make it better for some editors

--- OpenGLApp.cpp
+++ OpenGLApp.cpp
@@ -24,11 +24,11 @@ int main()
     {
         return winRet;
     }
-    ShaderStore::add_shader("default", "shaders/default.vs", "shaders/default.fs");
-    ShaderStore::add_shader("uvVisual", "shaders/default.vs", "shaders/texCoordVisual.fs");
-    ShaderStore::add_shader("normalVisual", "shaders/default.vs", "shaders/normalVisual.fs");
-    ShaderStore::add_shader("noLight", "shaders/default.vs", "shaders/noLight.fs");
-    ShaderStore::add_shader("shadow", "shaders/shadowMap.vs", "shaders/shadowMap.fs");
+    ShaderStore::add_shader("default", "shaders/default.vert", "shaders/default.frag");
+    ShaderStore::add_shader("uvVisual", "shaders/default.vert", "shaders/texCoordVisual.frag");
+    ShaderStore::add_shader("normalVisual", "shaders/default.vert", "shaders/normalVisual.frag");
+    ShaderStore::add_shader("noLight", "shaders/default.vert", "shaders/noLight.frag");
+    ShaderStore::add_shader("shadow", "shaders/shadowMap.vert", "shaders/shadowMap.frag");
 
     window->create_objects();
 
